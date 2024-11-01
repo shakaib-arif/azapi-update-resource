3d1fa83676b527460296e984c629d48be2a0c468
shakaib-arif <shakaib-arif@users.noreply.github.com>
2024-11-01 16:05:09 +0500

    formatting fixed

--- azapi-update-resource/main.tf
+++ azapi-update-resource/main.tf
@@ -1,5 +1,5 @@
 resource "azapi_update_resource" "this" {
-  for_each    = { for idx, value in var.update_resource_params : idx => value }
+  for_each                = { for idx, value in var.update_resource_params : idx => value }
   resource_id             = each.value.resource_id != null ? each.value.resource_id : provider::azapi::build_resource_id(each.value.parent_id, each.value.type, each.value.name)
   type                    = each.value.type # Required
   body                    = each.value.body # Required
