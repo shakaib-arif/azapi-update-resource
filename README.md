b091495a0e70aa46ecd41313107227dc20175450
shakaib-arif <shakaib-arif@users.noreply.github.com>
2024-11-01 16:04:54 +0500

    workaround configured per GH issue

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ Terraform resource `azapi_update_resource` module with its usage and example.
 ## Example 01
 
 - Use the latest `azapi` provider version
-    - module's for_each value `null` is not processed and throws error unless set explicitly.
-- not functional
+- functional code
 
 ## Example 02
 
 - Use old version `1.12.1` of `azapi`.
 - functional code
+
+## Reference
+
+- [azapi - resolved issue](https://github.com/Azure/terraform-provider-azapi/issues/656)
--- azapi-update-resource/main.tf
+++ azapi-update-resource/main.tf
@@ -1,8 +1,6 @@
 resource "azapi_update_resource" "this" {
-  for_each                = var.update_resource_params
-  name                    = each.value.name      # null
-  parent_id               = each.value.parent_id # null
-  resource_id             = each.value.resource_id
+  for_each    = { for idx, value in var.update_resource_params : idx => value }
+  resource_id             = each.value.resource_id != null ? each.value.resource_id : provider::azapi::build_resource_id(each.value.parent_id, each.value.type, each.value.name)
   type                    = each.value.type # Required
   body                    = each.value.body # Required
   response_export_values  = each.value.response_export_values
